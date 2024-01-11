c11a6254d46cae1c6c3a16c3af5ed6cec068c4f0
Ubirajara Neves <biraneves@outlook.com>
2024-01-11 14:28:13 +0100

    Comportamento do menu hambúrguer e dos botões

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $('div.menu-hamburguer').on('click', () => {
+        $('nav').slideToggle();
+    });
+
+    $('nav a').on('click', function () {
+        const destino = $($(this).attr('href'));
+
+        $('html').animate({
+            scrollTop: destino.offset().top - 104,
+        }, 500);
+    });
+
+    $('button').on('click', function () {
+        const produto = $(this).parent().find('h3').text();
+
+        alert(`Você adicionou ${produto} ao carrinho!`);
+    });
+});
